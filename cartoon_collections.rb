fd8eab7e717e04387f9d326b3565554ebd614924
Paul Jung <pbnjcub@gmail.com>
2023-02-09 19:58:51 -0500

    commit tree 32775206d6da0bbdb4bc36bf2d90bd6de9df923b

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,3 +19,33 @@ def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find {|food| cheese_types.include?(food)}
 end
+
+
+
+
+
+
+
+
+
+# def roll_call_dwarves(array)
+#   array.each_with_index do |name, index|
+#     puts "#{index + 1}. #{name}"
+#   end
+# end
+
+# def summon_captain_planet(array)
+#   array.map do |calls|
+#     calls.capitalize() << "!"
+#   end
+# end
+
+# def long_planeteer_calls(array)
+#   array.one? {|element| element.length > 4}
+# end
+
+# def find_the_cheese(array)
+#   # the array below is here to help
+#   cheese_types = ["cheddar", "gouda", "camembert"]
+#   array.find {|food| cheese_types.include?(food)}
+# end
